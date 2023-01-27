e1ba84cc5f37007ca635f65810d35a289fb28008
YuriyEG <yuriygermanovich0202@gmail.com>
2023-01-28 02:51:47 +0300

    закомитил аккордеон

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import Page from './page';
 import ShowBuildPhoto from './Showbuildphoto';
 
 
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 
 
 
@@ -17,6 +37,21 @@ function App() {
       
   return (
       <div>
+
+<button class="accordion">Section 1</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
             <div className='header'>
                   <div className='modul'>
                         ЖИЛЫЕ МОДУЛИ
